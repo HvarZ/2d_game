578b590420b75ea6b3e904cbc177a0eae65a2700
Hvarz <hvarzahar@gmail.com>
2021-01-26 02:36:49 +0300

    deleting dependence on CPU in moving main_hero

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,6 +7,8 @@ int main () {
 
     float current_frame = 0;
 
+    sf::Clock clock;
+
     sf::Sprite main_hero;
     main_hero.setTexture(texture);
     main_hero.setTextureRect(sf::IntRect(0, 244, 40, 50));
@@ -14,6 +16,10 @@ int main () {
 
     while (window.isOpen()) {
         sf::Event event{};
+
+        auto time = clock.getElapsedTime().asMicroseconds() / 200;
+        clock.restart();
+
         while (window.pollEvent(event)) {
             if ((event.type == sf::Event::Closed)
                 || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
@@ -21,17 +27,17 @@ int main () {
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            main_hero.move(-0.1, 0);
+            main_hero.move(-0.1f * time, 0);
 
-            if (current_frame += 0.005, current_frame > 6) current_frame -= 6;
+            if (current_frame += (0.005f * time), current_frame > 6) current_frame -= 6;
 
             main_hero.setTextureRect(sf::IntRect(40 * int(current_frame) + 40, 244, -40, 50));
         }
 
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            main_hero.move(0.1, 0);
+            main_hero.move(0.1f * time, 0);
 
-            if (current_frame += 0.005, current_frame > 6) current_frame -= 6;
+            if (current_frame += (0.005f * time), current_frame > 6) current_frame -= 6;
 
             main_hero.setTextureRect(sf::IntRect(40 * int(current_frame), 244, 40, 50));
         }
