de692dfd7bcd18dd29bc89c503aea53902e4f72f
Hvarz <hvarzahar@gmail.com>
2021-01-30 02:12:11 +0300

    add final and explicit in level_1

--- include/level_1.hpp
+++ include/level_1.hpp
@@ -4,12 +4,12 @@
 #include <vector>
 #include <string>
 
-class Level_1 {
+class Level_1 final {
 private:
     std::vector<std::string> map_;
 
 public:
-    Level_1(const std::vector<std::string>& map) noexcept;
+    explicit Level_1(const std::vector<std::string>& map) noexcept;
 
     void printMap() const noexcept;
 };
--- sources/level_1.cpp
+++ sources/level_1.cpp
@@ -2,3 +2,6 @@
 
 Level_1::Level_1(const std::vector<std::string>& map) noexcept : map_(map) {}
 
+void Level_1::printMap() const noexcept {
+    // in progress
+}
